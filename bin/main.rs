417eb811472d3b8ab268e87a5dbed401af892fbe
Ryan Chandler <ryangjchandler@gmail.com>
2021-05-26 21:36:44 +0100

    wip

--- bin/main.rs
+++ bin/main.rs
@@ -4,7 +4,14 @@ use tusk_lexer::Lexer;
 use tusk_parser::Parser;
 
 fn main() {
-    let filepath = env::args().nth(1).unwrap();
+    let filepath = match env::args().nth(1) {
+        Some(filepath) => filepath,
+        None => {
+            println!("Please provide a filepath to generate an AST.");
+            std::process::exit(1);
+        }
+    };
+
     let contents = fs::read_to_string(filepath).unwrap();
 
     let lexer = Lexer::new(contents.as_str());
